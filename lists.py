0cadb96de1fe4f2ec15677fd07f4ace234308255
andrew <abdumalikovfourth@gmail.com>
2026-04-27 00:40:59 +0900

    feat: practice lambda function

--- lists.py
+++ lists.py
@@ -27,8 +27,8 @@ fruits = ["grape", "apple", "melon", "kiwi", "cherry", "orange"]
 
 a = fruits[0]
 b = fruits[0:2]
-c = fruits[1::3]
-d = fruits[2::-2]
+c = fruits[3::2]
+d = fruits[5::-2]
 
 print("a: {}, b: {}, c: {}, d: {}".format(a, b, c, d))
 
@@ -40,10 +40,11 @@ letters = ["a", "b", "d"]
 letters.append("c")  # add element to end
 print(f"append letters: {letters}")
 
-letters.insert(0, "z")  # add element to before the value at 0 index
+letters.insert(2, "z")  # add element to before the value at 0 index
 print(f"insert letters: {letters}")
 
 size = len(letters) - 1
+
 r1 = letters.pop(size)  # pop behind
 print(f"the pop result1: {r1} and letters: {letters}")
 
@@ -56,7 +57,7 @@ print("------")
 animals = ["fish", "cat", "dog", "lion", "tiger"]
 print("animals:", animals)
 
-animals.remove("fish")
+animals.remove("dog")
 print("remove animals:", animals)
 
 del animals[2:4]
@@ -66,7 +67,7 @@ exist = animals.index("cat")
 print("cat exists:", exist)
 
 # animals.clear()
-# print(animals)
+# print("animals: ", animals)
 
 if "cat" in animals:
     print("index of cat:", animals.index("cat"))
@@ -75,7 +76,9 @@ else:
 
 
 print("------")
+
 numbers = [2, 12, 8, 5, 34, 23]
+
 numbers.sort()
 print("sort default:", numbers)
 numbers.sort(reverse=True)
@@ -85,4 +88,29 @@ print("sort reverse:", numbers)
 numbs = [34, 756, 1, 2]
 new_nums = sorted(numbs)
 print(f"numbs: {numbs} and new_nums: {new_nums}")
+
+
+print("\n===== Lambda function =====")
+# lambda is small anonymous function!
+
+def calculate(x, y): return x * y
+
+result = calculate(3, 5)
+print("result:", result)
+
+# lambdaning asosiy kuchi > ikkala valueni ham alohida chiqarib beradi
+people = [
+    ("Robert", 20),
+    ("Steve", 4),
+    ("Joseph", 25),
+    ("Michael", 1),
+    ("Ali", 40)
+]  
+
+people.sort()
+print("people1:", people)
+
+# sort by age via lambda function
+people.sort(key = lambda person: person[1])
+print("people2:", people)
 
