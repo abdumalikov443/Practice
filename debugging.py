cb0980de249d3dad2405fa5a8ecfbb9003835db7
andrew <abdumalikovfourth@gmail.com>
2026-04-28 23:47:33 +0900

    feat: Core Packages study

---
+++ debugging.py
@@ -0,0 +1,33 @@
+''' Packages & Debugging
+    (1) Python Packages & Core Packages
+    (2) Package Manager & External Package
+    (3) Debugging
+'''
+
+import turtle
+print("===== Python Packages & Core Packages =====")
+''' Python Packages/Modules: Core, File and External '''
+# Core Packages > https://docs.python.org/3/library
+
+# Core package
+# t = turtle.Turtle()
+# t.shape("turtle")
+# t.speed(2)
+# t.circle(150)
+# turtle.done()
+
+print("-----")
+
+my_file = open("material/sample.txt", "r")
+try:
+    content = my_file.read()
+    print(content)
+finally:
+    my_file.close()
+
+# with - Context Manager
+with open("material/sample.txt", "r") as your_file:
+    your_content = your_file.read()
+    print("your_content:", your_content)
+
+print("Done")
