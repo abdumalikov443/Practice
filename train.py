7a2e5339b97c128938c7621a64ad9fcb8dcb709e
andrew <abdumalikovfourth@gmail.com>
2026-05-05 20:56:38 +0900

    fix: I-TASK solved

--- train.py
+++ train.py
@@ -1,3 +1,24 @@
+''' I-TASK (PYTHON)
+
+    ⭐ Savol: Shunday function tuzing, unga string argument pass bolsin. 
+    Function ushbu agrumentdagi digitlarni yangi stringda return qilsin
+    MASALAN: get_digits("m14i1t") return qiladi "141"
+'''
+
+''' Masalaning yechimi '''
+def get_digits(str):
+    nums = []
+    for i in str:
+        if '0' < i < '9':
+            nums.append(i)
+    return ''.join(nums)
+
+
+result = get_digits("m14i1t") 
+print(f'The result: {result}')
+
+
+
 ''' G-TASK (PYTHON)
 
     ⭐ Savol: Shunday function tuzingki unga integerlardan iborat array pass bolsin 
@@ -6,13 +27,13 @@
 '''
 
 ''' Masalaning yechimi: '''
-def max_num_index(arr):
-    max_num = arr[0]
-    for num in arr:
-        if num > max_num:
-            # print(num)
-            max_num = num
-    return arr.index(max_num)
+# def max_num_index(arr):
+#     max_num = arr[0]
+#     for num in arr:
+#         if num > max_num:
+#             # print(num)
+#             max_num = num
+#     return arr.index(max_num)
 
-result = max_num_index([5, 21, 12, 21, 56, 8, 43])
-print(f"The highest number of index is: {result}")
+# result = max_num_index([5, 21, 12, 21, 56, 8, 43])
+# print(f"The highest number of index is: {result}")
